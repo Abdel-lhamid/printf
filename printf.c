1a4927d9362f38be74c28006881d3d0c0243bd74
ABDELHAMID El Yaagoubi <abdelhamid.business@gmail.com>
2023-07-23 13:48:22 -0700

    reversed last changes

--- printf.c
+++ printf.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 			{
 				char_p += printed_idf;
 			}
-			if (printed_idf == 0 || printed_idf == -1)
+			if (printed_idf == 0)
 			{
 				_putchar('%');
 				char_p++;
